97201e0854d9ba93da6cb065f42c3b32dc03e9a9
gaoxi <gaoxi@meicai.cn>
2019-05-22 11:52:08 +0800

    2.2.0：清理日志更新，日志添加详细信息

--- dockershare/clear_m3u8.sh
+++ dockershare/clear_m3u8.sh
@@ -1,2 +1,9 @@
-find /usr/local/srs/objs/nginx/html/ -type f -mtime +7 -delete
+files=`find /usr/local/srs/objs/nginx/html/ -type f -mtime +7`
 echo "$(date)-----M3u8 Cleaning Script is running!">>/usr/local/srs/objs/nginx/M3u8_clear.log
+for file in ${files[@]}
+do
+  file_data=`ls -lt $file`
+  echo "  $(date)-----${file_data} , File storage time more than 7 day , has been deleted!">>/usr/local/srs/objs/nginx/M3u8_clear.log
+done
+echo -e "\n">>/usr/local/srs/objs/nginx/M3u8_clear.log
+find /usr/local/srs/objs/nginx/html/ -type f -mtime +7 -delete
